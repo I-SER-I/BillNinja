b5a1b1b8fe6280e03291c1e592e7cf949e69b256
Sergei Papikian <I_SER_I@hotmail.com>
2022-07-10 00:21:08 +0300

    Add project files.

---
+++ BillNinja/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BillNinja;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BillNinja/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BillNinja;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
